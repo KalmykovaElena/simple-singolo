01c09e7925b6f4bbf6aa9dbd88b254b02df8aadf
Liudmila111983 <kalmukhovae@gmail.com>
2022-03-31 20:15:58 +0300

    change burger

--- script.js
+++ script.js
@@ -1,66 +1,73 @@
-const prev = document.querySelector('.arrow-left'),
-      next = document.querySelector('.arrow-right'),
-      slides = document.querySelectorAll('.slider'),
-      dots = document.querySelectorAll('.dot');
-      let index = 0;
+const prev = document.querySelector(".arrow-left"),
+  next = document.querySelector(".arrow-right"),
+  slides = document.querySelectorAll(".slider"),
+  dots = document.querySelectorAll(".dot");
+let index = 0;
 
-      const changeActive=(ind)=>{
-          slides.forEach(el => {
-              el.classList.remove('active');
-          });
-          slides[ind].classList.add('active')
-      }
-      
-      const nextSlide =()=>{
-          if(index == slides.length - 1){
-              index=0;
-              changeActive(index);
-              changeActiveDots(index)
-          }else{
-              index++;
-              changeActive(index);
-              changeActiveDots(index)
-          }
-      }
-      const prevSlide =()=>{
-          if(index==0){
-              index= slides.length - 1
-              changeActive(index);
-              changeActiveDots(index)
-          }else{
-              index--;
-              changeActive(index);
-              changeActiveDots(index)
-          }
-      }
-      next.addEventListener('click',nextSlide);
-      prev.addEventListener('click',prevSlide);
+const changeActive = (ind) => {
+  slides.forEach((el) => {
+    el.classList.remove("active");
+  });
+  slides[ind].classList.add("active");
+};
 
-      window.addEventListener('keydown',(event)=>{
-        if (event.keyCode == 39){
-            console.log(11);
-            nextSlide()
-        }else if(event.keyCode == 37){
-            prevSlide()
-        }
-      });
-      const changeActiveDots=(ind)=>{
-        dots.forEach(el => {
-            el.classList.remove('active');
-        });
-        dots[ind].classList.add('active')
-    }
-      dots.forEach((el,ind)=>{
-          el.addEventListener('click',()=>{
-              index=ind;
-              changeActive(index);
-              changeActiveDots(index);
-          })
-      })
+const nextSlide = () => {
+  if (index == slides.length - 1) {
+    index = 0;
+    changeActive(index);
+    changeActiveDots(index);
+  } else {
+    index++;
+    changeActive(index);
+    changeActiveDots(index);
+  }
+};
+const prevSlide = () => {
+  if (index == 0) {
+    index = slides.length - 1;
+    changeActive(index);
+    changeActiveDots(index);
+  } else {
+    index--;
+    changeActive(index);
+    changeActiveDots(index);
+  }
+};
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
 
-      const btn=document.querySelector('.burger-icon'),
-            menu = document.querySelector('.burger-menu');
+window.addEventListener("keydown", (event) => {
+  if (event.keyCode == 39) {
+    console.log(11);
+    nextSlide();
+  } else if (event.keyCode == 37) {
+    prevSlide();
+  }
+});
+const changeActiveDots = (ind) => {
+  dots.forEach((el) => {
+    el.classList.remove("active");
+  });
+  dots[ind].classList.add("active");
+};
+dots.forEach((el, ind) => {
+  el.addEventListener("click", () => {
+    index = ind;
+    changeActive(index);
+    changeActiveDots(index);
+  });
+});
 
-            btn.addEventListener('click',()=>{
-                menu.classList.toggle('active-burger-menu')
-            })
+const btn = document.querySelector(".burger-icon"),
+  menu = document.querySelector(".burger-menu"),
+  anchor = document.querySelectorAll(".navigation--list");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active-burger-menu");
+});
+
+anchor.forEach((el) => {
+  el.addEventListener("click", () => {
+    menu.classList.remove("active-burger-menu");
+  });
+});
